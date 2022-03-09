c6504626025c1e6d8662c08bc9bcc9b5424f6131
Brittany Ganser <15brganser@gmail.com>
2022-03-09 12:20:10 -0700

    creted fetch function to retrieve github user repositories

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
-};
+//user is a parameter...you can  pick a different username when calling the function
+var getUserRepos = function (user) {
+  // format the github api url..user is the user that you pass as an arguement
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+//you can plug in different usernames as a string to view their repos
+getUserRepos("bganser15");
