d2f81e0a36bcebc8ab3250624007b41439d3745a
Brittany Ganser <15brganser@gmail.com>
2022-03-18 12:41:34 -0600

    accessed headers to see if more response data exists

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,11 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         //calling the function to add data to display
         displayIssues(data);
+
+        //check in headers if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -45,6 +51,15 @@ var displayIssues = function (issues) {
     issueContainerEl.appendChild(issueEl);
   }
 };
+var displayWarning = function (repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "Github.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+  limitWarningEl.appendChild(linkEl);
+};
 
-//padding the user and repo we want to access in getRepoIssues
-getRepoIssues("bganser15/coding-quiz");
+//adding the user and repo we want to access in getRepoIssues
+getRepoIssues("facebook/react");
